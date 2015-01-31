3b135ef7a6739e2d692b41124f07160559832011
ANDRES KWAN <andreskwan@gmail.com>
2015-01-31 18:36:40 -0500

    4.7 - directive + controller - gallery

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -12,11 +12,7 @@
 
 
   app.controller('GalleryController', function(){
-    this.current = 0;
-
-    this.setCurrent = function(imageNumber){
-      this.current = imageNumber || 0;
-    };
+    
   });
 
   
@@ -26,6 +22,21 @@
       product.reviews.push(this.review);
       this.review = {};
     };  
+  });
+
+  app.directive("productGallery", function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'partials/product-gallery.html',
+      controller: function(){
+          this.current = 0;
+
+          this.setCurrent = function(imageNumber){
+            this.current = imageNumber || 0;
+          };
+      },
+      controllerAs: 'gallery'
+    };
   });
 
   app.directive("productTabs", function() {
