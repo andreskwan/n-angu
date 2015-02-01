1beac4cb2956eb077f53c8e76fe420b1f353cfd9
ANDRES KWAN <andreskwan@gmail.com>
2015-01-31 19:13:04 -0500

    5.2 - refactoring - using dependencies (modules)

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,5 +1,5 @@
 (function() {
-  var app = angular.module('gemStore', []);
+  var app = angular.module('gemStore', ['store-products']);
 
   app.config(function($interpolateProvider) {
     $interpolateProvider.startSymbol('%%');
@@ -9,78 +9,8 @@
   app.controller('StoreController', function() {
     this.products = gems;
   });
-
-
-  app.controller('GalleryController', function(){
-    
-  });
-
   
-  app.controller("ReviewController", function(){
-    this.review = {};
-    this.addReview = function(product){
-      product.reviews.push(this.review);
-      this.review = {};
-    };  
-  });
-
-  app.directive("productGallery", function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'partials/product-gallery.html',
-      controller: function(){
-          this.current = 0;
-
-          this.setCurrent = function(imageNumber){
-            this.current = imageNumber || 0;
-          };
-      },
-      controllerAs: 'gallery'
-    };
-  });
-
-  app.directive("productTabs", function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'partials/product-tabs.html',
-      controller: function(){
-          this.tab = 1;
-
-          this.isSet = function(checkTab) {
-            return this.tab === checkTab;
-          };
-
-          this.setTab = function(setTab) {
-            this.tab = setTab;
-          };
-      },
-      controllerAs: 'tab'
-    };
-  });
-
-  app.directive("productDescription", function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'partials/product-description.html'
-      // template: '<h4>Description</h4> <blockquote>{{product.description}}</blockquote>'
-    };
-  });
-
-  app.directive("productSpecs", function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'partials/product-specs.html'
-    };
-  });
-
-  app.directive("productReviews", function() {
-    return {
-      restrict: 'E',
-      templateUrl: "partials/product-reviews.html"
-    };
-  });
-
-var gems = [{
+  var gems = [{
   name: 'Azurite',
   description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
   shine: 8,
