5cfe187d902ffd320188b2976d1bff6c1b5f2a2d
ANDRES KWAN <andreskwan@gmail.com>
2015-03-27 17:51:18 -0500

    ng-options - role - working

--- builds/development/js/user-controller.js
+++ builds/development/js/user-controller.js
@@ -5,35 +5,32 @@
 			return $resource(Constantes.url+"/usuarios/:id/")
 			// return $resource("/productos/:id/");
 		}])
-	.controller('UserController',['$http', '$scope', 'Constantes','UserFactory',
-		function($http, $scope, Constantes, UserFactory){
-			// this.perfil =
-			// $scope.perfil = {"name":"andres"};
-			// alert("Welcome");
-			// debugger;
-			// var datosQuery = UserFactory.query(function (){
-			// 	// $scope.datos = datosQuery;
-			// 	console.log(datosQuery);
-			// });
+	.factory("RoleFactory",['$resource', 'Constantes',
+		function($resource, Constantes){
+			return $resource(Constantes.url+"/roles/:id/")
+			// return $resource("/productos/:id/");
+		}])
+	.controller('UserController',['$http', '$scope', 'Constantes','UserFactory','RoleFactory',
+		function($http, $scope, Constantes, UserFactory, RoleFactory){
 
-			// var datosGet = UserFactory.get({id:2}, function(){
-			// 	// $scope.datos = datosGet;
-			// 	console.log(datosGet);
-			// });
-
-			// console.log("gemStore.Constantes:",gemStore.Constantes)
 			$scope.master = {};
 
+	        RoleFactory.query(function (roles){
+	        	// $scope.roles = [{id:"1","name":"Empresario"},{id:"2","name":"Empre"}];
+	        	// debugger;
+	        	$scope.roles = roles;
+	        },function (error){
+	        	console.log("Error.status",error.status);	
+	        });
 	      $scope.update = function(user) {
-	      	// debugger;
 	      	// product = {"producto":user};
-        	// console.log(product);
+        	console.log(user);
+	      	debugger;
 	        UserFactory.save(function(user){
 	        	console.log(postResult);
 	        },function (error){
 	        	console.log("Error.status",error.status);
-	        })
-
+	        });
 
 	        // $http.post('/productos', product).
          //        success(function(data, status, headers, config) {
