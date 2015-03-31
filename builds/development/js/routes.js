3ca36f07bd51ecb5285be30639bf3cdab150761a
ANDRES KWAN <andreskwan@gmail.com>
2015-03-31 12:19:49 -0500

    ONLY - UNAL - angular ROUTES

--- builds/development/js/routes.js
+++ builds/development/js/routes.js
@@ -7,25 +7,10 @@
   .config(['$routeProvider',function($routeProvider) {
     // debugger;
     $routeProvider.
-      when('/create',
-      {
-        templateUrl: 'templates/products/product-create.html',
-        controller: 'ProductCreateController'
-      }).
       when('/user',
       {
         templateUrl: 'templates/user/user.html',
         controller: 'UserController'
-      }).
-      when('/products',
-      {
-        templateUrl: 'templates/products/products.html',
-        controller: 'ProductsController'
-      }).
-      when('/products/:id',
-      {
-        templateUrl: 'templates/products/product.html',
-        controller: 'ProductController'
       }).
       when('/index',
       {
@@ -36,7 +21,7 @@
         templateUrl: 'templates/unal.html',
       }).
       otherwise({
-        redirectTo:'/products'
+        redirectTo:'/user'
       });
   }]);
 })();
