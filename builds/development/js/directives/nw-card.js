f22f272c1929925d182890cfcd07955e12d7e68f
ANDRES KWAN <andreskwan@gmail.com>
2015-04-11 14:07:17 -0500

    L3.3 - factory - teste for POST

--- builds/development/js/directives/nw-card.js
+++ builds/development/js/directives/nw-card.js
@@ -1,25 +1,19 @@
 (function(){
 	angular.module('gemStore')
 	.directive('nwCardDirective', [function(){
-		// Runs during compile
 		return {
-			// name: '',
-			// priority: 1,
-			// terminal: true,
+			restrict: 'E',
+			templateUrl: 'templates/directives/nw-card.html',
 			scope: {
-				header : "=",
-				description : "="
-
-			}, // {} = isolate, true = child, false/undefined = no change
-			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-			restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
-			// template: '',
-			templateUrl: 'templates/directives/nw-card.html'
-			// replace: true,
-			// transclude: true,
-			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
-			// link: function($scope, iElm, iAttrs, controller) {
-			// }
+				header     : "=",
+				description: "=",
+				tweeted    : '='
+			},
+			link: function($scope, element) {
+				if(scope.tweeted){
+					element.addClass('tweeted');
+				}
+			}
 		};
 	}]);
 })();
