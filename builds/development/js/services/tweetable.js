4ac23487995c8c116ec5777477159d96861d9309
ANDRES KWAN <andreskwan@gmail.com>
2015-04-11 16:35:16 -0500

    L3.6 - The Configurable Bits

--- builds/development/js/services/tweetable.js
+++ builds/development/js/services/tweetable.js
@@ -1,11 +1,13 @@
 (function(){
 	angular.module('gemStore')
 	.factory('TweetableFactory', ['$http', function($http){
+		var characterLength = 100;
 		return function name(potentialTweet){
 			return $http({ 
 							method : "POST", 
 							url  : "http://gentle-spire-1153.herokuapp.com/tweet", 
-							data : {description:potentialTweet}
+							data : {description:potentialTweet},
+							maxLength: characterLength
 						});
 		};
 	}]);
